abeb68889861ab4150d31c3f8836ccc836e2b0a5
Goran Sinovčić <gsinovcic@foi.hr>
2022-02-03 23:51:56 +0100

    init

---
+++ GameOfLife-VAS/Creature.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife_VAS
+{
+    public class Creature
+    {
+        //stanje jedinke
+        public int ziv { get; set; }
+        //broj susjeda jedinke
+        public int brSusjeda { get; set; }
+        //lokacija jedinke
+        public int[] lokacija { get; set; }
+        //koliko generacija je preživjela
+        public int starost { get; set; }
+
+        public void NadiSusjede(Creature[,] creatures,int gridSize)
+        {
+            
+            brSusjeda = 0;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    brSusjeda += creatures[(lokacija[0] + i + gridSize) % gridSize, (lokacija[1] + j + gridSize) % gridSize].ziv;
+                }
+            }
+            brSusjeda -= creatures[lokacija[0], lokacija[1]].ziv;
+        }
+    }
+}
---
+++ GameOfLife-VAS/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace GameOfLife_VAS
+{
+    public partial class MainWindow : Window
+    {
+        public const int gridSize = 50;
+
+        Uri uriSource = new Uri("/Kuca.png", UriKind.Relative);
+        Random RNG = new Random();
+        DispatcherTimer timer = new DispatcherTimer();
+        Image[,] polja = new Image[gridSize, gridSize];
+        Creature[,] creatures = new Creature[gridSize, gridSize];
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            kuca.Source = new BitmapImage(uriSource);
+            grid.Arrange(new Rect(0.0, 0.0, grid.DesiredSize.Width, grid.DesiredSize.Height));
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    var rand = RNG.Next(0, 2) == 1;
+                    Image img = new Image();
+                    Creature c = new Creature();
+                    img.Width = grid.ActualWidth / gridSize;
+                    img.Height = grid.ActualHeight / gridSize;
+                    img.Source = rand ? new BitmapImage(uriSource) : null;
+                    c.ziv = rand ? 0 : 1;
+                    c.lokacija = new int[] { i, j };
+                    polja[i, j] = img;
+                    creatures[i, j] = c;
+                    Canvas.SetTop(img, i * grid.ActualHeight / gridSize);
+                    Canvas.SetLeft(img, j * grid.ActualWidth / gridSize);
+                    grid.Children.Add(img);
+                }
+            }
+            timer.Start();
+            timer.Tick += Next_gen;
+        }
+
+        int[] maxLoc;
+        int brojGen;
+
+        private void Next_gen(object sender, EventArgs e)
+        {
+            int maxStarost = 0;
+            int trZivih = 0;
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    creatures[i, j].NadiSusjede(creatures, gridSize);
+                }
+            }
+
+            for (int i = 0; i < gridSize; i++)
+            {
+                for (int j = 0; j < gridSize; j++)
+                {
+                    if (creatures[i, j].brSusjeda < 2 || creatures[i, j].brSusjeda > 3)
+                    {
+                        creatures[i, j].ziv = 0;
+                        creatures[i, j].starost = 0;
+                        polja[i, j].Source = null;
+
+                    }
+                    else if (creatures[i, j].brSusjeda == 3)
+                    {
+
+                        creatures[i, j].ziv = 1;
+                        creatures[i, j].starost++;
+                        polja[i, j].Source = new BitmapImage(uriSource);
+                        trZivih++;
+                    }
+                    if (creatures[i, j].starost >= maxStarost)
+                    {
+                        maxStarost = creatures[i, j].starost;
+                        maxLoc = new int[] { i, j };
+                    }
+
+                }
+            }
+            curGen.Content = "Trenutna generacija: " + brojGen++;
+            starostLbl.Content = maxStarost.ToString()+" generacija.";
+            aliveLbl.Content = "Trenutno živih agenata: " + trZivih;
+            polja[maxLoc[0], maxLoc[1]].Source = new BitmapImage(new Uri("/Kuca_crvena.png", UriKind.Relative));
+            Thread.Sleep(100);
+
+        }
+
+       
+    }
+}
